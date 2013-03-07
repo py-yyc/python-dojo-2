5c23f13eaedf90cc17f9740c3021f76f36753fd4
Abhin Chhabra <chhabra.abhin@gmail.com>
2013-03-06 20:02:10 -0700

    Added a POST handling logic

--- deutchebags/main.py
+++ deutchebags/main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+GET = "GET"
+POST = "POST"
+BOARD_SIZE = 3
+
+@app.route("/", methods=[GET, POST])
 def mark():
-    board = [[''] * 3] * 3
+    board = [[''] * BOARD_SIZE] * BOARD_SIZE
+    if request.method == POST:
+        for row_idx in range(BOARD_SIZE):
+            for col_idx in range(BOARD_SIZE):
+                form_element_id = "{row}-{col}".format(row=row_idx, col=col_idx)
+                board[row_idx][col_idx] = request.form[form_element_id]
     return render_template('board.html', board=board)
 
 if __name__ == "__main__":
